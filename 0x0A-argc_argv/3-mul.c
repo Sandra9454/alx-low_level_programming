842516d1fb2c6bd759da462821dea0f34d70bf5e
Sandra9454 <sandra.shacak@gmail.com>
2023-11-01 11:44:11 -0700

    Task 4

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,20 +1,17 @@
 #include "main.h"
-#include <stdlib.h>
 #include <stdio.h>
 
 /**
  * main - a program that multiplies two numbers.
- * @num1: first number
- * @num2: 2nd number
  * @argc: argument coun
  * @argv: argument vector
  */
 
 int main(int argc, char *argv[])
 {
-	int num1;
-	int num2;
-	int product;
+	int num1 = 0;
+	int num2 = 0;
+	int output;
 
 	if (argc != 3)
 	{
@@ -25,9 +22,9 @@ int main(int argc, char *argv[])
 
 	int num1 = atoi(argv[1]);
 	int num2 = atoi(argv[2]);
-	int product = num1 * num2;
+	int output = num1 * num2;
 
-	printf("%d\n", product);
+	printf("%d\n", output);
 
 	return (0);
 }
