d8ea75771222696f7a6b235be707c53b106da3af
Sandra9454 <sandra.shacak@gmail.com>
2023-11-08 04:45:20 -0800

    Task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dogs' basic info
+ * @age: dog age
+ * @name: dog name
+ * @owner: dog owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
