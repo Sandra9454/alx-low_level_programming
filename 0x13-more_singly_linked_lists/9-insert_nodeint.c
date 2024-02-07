50df904c316ca15bf2c4e77b5d76d45ea8e03c59
Sandra Saad <sandra.shacak@gmail.com>
2024-02-07 12:04:43 -0800

    Task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -50,7 +50,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 
 	if (idx == 0)
-		* head = new_node;
+		*head = new_node;
 	for (i = 0; i < idx - 1 && tmp != NULL && idx != 0; i++)
 		tmp = tmp->next;
 	if (tmp == NULL)
