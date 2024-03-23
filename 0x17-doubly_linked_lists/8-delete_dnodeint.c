387933aa44c9b0052856f8ffac47956076480bf5
Sandra9454 <sandra.shacak@gmail.com>
2024-03-23 08:43:26 -0700

    Task 8

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -33,6 +33,8 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		i++;
 	}
 
+	if (current == NULL)
+		return (-1);
 	if (current->next != NULL)
 	{
 		current->next->prev = temp;
